abcb793c5486bf4984e3359e07e20996c7ff1069
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-27 00:41:13 -0500

    actu diez

--- pharmacy_sales_system/backend/accounts/admin.py
+++ pharmacy_sales_system/backend/accounts/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Información personal'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permisos'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Fechas importantes'), {'fields': ('last_login', 'date_joined')}),
+        (_('Rol personalizado'), {'fields': ('rol',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'rol'),
+        }),
+    )
+    list_display = ('username', 'first_name', 'last_name', 'rol', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name')
+    ordering = ('username',)

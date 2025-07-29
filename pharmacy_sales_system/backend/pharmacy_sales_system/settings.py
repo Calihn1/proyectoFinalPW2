0e5725f5fd4fed28d6283cbdfc6731fcab12837c
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-29 17:20:03 -0500

    actu veintiocho

--- pharmacy_sales_system/backend/pharmacy_sales_system/settings.py
+++ pharmacy_sales_system/backend/pharmacy_sales_system/settings.py
@@ -171,3 +171,11 @@ AUTH_USER_MODEL = 'accounts.User'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'engrf4bricio@gmail.com'
+EMAIL_HOST_PASSWORD = 'vixs exim rqqu qqmw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
